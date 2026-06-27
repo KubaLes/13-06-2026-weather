df9da3c6ae501d89413c0f2e9555001805ab2469
Your NameKubaLes <jakles1994@wp.pl>
2026-06-27 13:37:50 +0200

    msql connection

--- config.py
+++ config.py
@@ -6,3 +6,7 @@ class Config:
     API_KEY = os.getenv("ENV_API_KEY")
     PLACE = os.getenv("ENV_PLACE")
     WEATHER_FILE=os.getenv("ENV_WEATHER_FILE")
+    DB_HOST=os.getenv("ENV_DBHOST")
+    DB_USER=os.getenv("ENV_DBUSER")
+    DB_PASSWORD=os.getenv("ENV_DBPASSWORD")
+    DB_DATABASE=os.getenv("ENV_DBDATABASE")
